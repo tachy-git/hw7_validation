eaa9c0ffb54caea5ef158a0c40a93003a70c3967
Taehee Kim <eikz.il2ljlpl@gmail.com>
2025-01-03 17:21:18 +0900

    change the settings for kisti server

--- FullShower/RKZp/13TeV/QCD_M5_13TeV-mg-hw/submit_condor_hw.sh
+++ FullShower/RKZp/13TeV/QCD_M5_13TeV-mg-hw/submit_condor_hw.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-#SAMPLES=("Pt-130To135_1132246" "Pt-135To140_1132294" "Pt-140To150_1132295" "Pt-150To160_1132248" "Pt-160To170_1132249" "Pt-170To200_1132250" "Pt-200To9999_1132251")
-#SAMPLES=("Pt-200To210_1132252" "Pt-210To220_1132253" "Pt-220To230_1132254" "Pt-230To250_1132255" "Pt-250To280_1132256" "Pt-280To350_1132257" "Pt-350To9999_1132258")
-#SAMPLES=("Pt-230To240_1132296" "Pt-240To250_1132297" "Pt-250To270_1132298" "Pt-270To300_1132299" "Pt-300To350_1132300" "Pt-350To9999_1132258")
-SAMPLES=("Pt-130To135_345510" "Pt-135To140_345511" "Pt-140To150_345512" "Pt-150To160_345513" "Pt-160To170_345514" "Pt-170To180_345515" "Pt-180To200_345516" "Pt-200To250_345517" "Pt-250To9999_345518")
-SAMPLES=("Pt-230To240_345524" "Pt-240To250_345525" "Pt-250To260_345526" "Pt-260To280_1132311" "Pt-280To300_1132312" "Pt-300To350_1132313" "Pt-350To400_1132314" "Pt-400To9999_1132315")
+SAMPLES=("Pt-130To140_4720322" "Pt-140To150_4720323" "Pt-150To170_4720324" "Pt-170To200_4720325" "Pt-200To250_4720326" "Pt-250To9999_4720327")
 
-zpmass=55
+zpmass=50
 coupling="0.1"
 
 cat <<EOT > submit_condor_hw.txt
@@ -15,19 +11,23 @@ executable      = hw_condor.sh
 arguments       = \$(Cluster) \$(Process) $sample $zpmass $coupling
 output          = joblog/job.\$(Cluster).\$(Process).out
 error           = joblog/job.\$(Cluster).\$(Process).err
-log             = joblog/job.log
-+SingularityImage = "/u/user/taehee/osgvo-ubuntu-20.04_latest.sif"
-+SingularityBind  = "/u/user/taehee/HerwigWD:/u/user/taehee/HerwigWD"
+log             = joblog/job.\$(Cluster).\$(Process).log
++SingularityImage = "/cms/ldap_home/taehee/osgvo-ubuntu-20.04_latest.sif"
++SingularityBind  = "/cms/ldap_home/taehee:/cms/ldap_home/taehee,/cms_scratch/taehee:/cms_scratch/taehee"
+accounting_group = group_alice
 stream_output = True
 stream_error = True
+should_transfer_files = YES
+getenv = True
 EOT
 
 for ((i=0; i<${#SAMPLES[@]}; i++)); do
     sample=${SAMPLES[i]}
-    queue=$(ls /pnfs/knu.ac.kr/data/cms/store/user/taehee/HerwigSample/mg/MZp-$zpmass/$sample | wc -l)
-	echo "Submitting job for HW with MG job number $sample with Zprime mass $zpmass, coupling $coupling"
+    queue=$(ls /cms_scratch/taehee/HerwigSample/mg_nEvt-1000/MZp-$zpmass/$sample | wc -l)
+    echo "Submitting $queue jobs for HW with MG job number $sample with Zprime mass $zpmass, coupling $coupling"
 	condor_submit submit_condor_hw.txt \
 	-append "arguments = \$(Cluster) \$(Process) $sample $zpmass $coupling" \
 	-append "JobBatchName = MZp-"$zpmass"_"$sample \
 	-append "queue $queue"
+	sleep 30
 done
--- FullShower/RKZp/13TeV/QCD_M5_13TeV-mg-hw/submit_condor_mg.sh
+++ FullShower/RKZp/13TeV/QCD_M5_13TeV-mg-hw/submit_condor_mg.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-#ptbin=(130 135 140 150 160 170 180 200 250 9999)
-#queues=(1684 261 3960 3466 928 671 1523 3157 2449)
-ptbin=(230 240 250 270 300 350 9999)
-queues=(4327 1750 6142 2963 6210 8322)
+ptbin=(130 140 150 170 200 250 9999)
 
-zpmass=55
+zpmass=50
 
 cat <<EOT > submit_condor_mg.txt
 universe        = vanilla
@@ -13,19 +10,21 @@ executable      = mg_condor.sh
 arguments       = \$(Cluster) \$(Process) \$(ptj) \$(ptjmax) \$(zpmass)
 output          = joblog/job.\$(Cluster).\$(Process).out
 error           = joblog/job.\$(Cluster).\$(Process).err
-log             = joblog/job.log
-+SingularityImage = "/u/user/taehee/HerwigLoc/osgvo-ubuntu-20.04_latest.sif"
-+SingularityBind = "/u/user/taehee:/u/user/taehee"
+log             = joblog/job.\$(Cluster).\$(Process).log
++SingularityImage = "/cms/ldap_home/taehee/osgvo-ubuntu-20.04_latest.sif"
++SingularityBind = "/cms/ldap_home/taehee:/cms/ldap_home/taehee,/cms_scratch/taehee:/cms_scratch/taehee"
+accounting_group = group_alice
 should_transfer_files = YES
+getenv = True
 EOT
 
 for ((i=0; i<${#ptbin[@]}-1; i++)); do
     ptj=${ptbin[i]}
     ptjmax=${ptbin[i+1]}
     queue=${queues[i]}
-    #queue=500
     echo "Submitting job for MG ptbinned [$ptj, $ptjmax] with Zprime mass $zpmass"
     condor_submit submit_condor_mg.txt \
     -append "arguments = \$(Cluster) \$(Process) $ptj $ptjmax $zpmass" \
+    -append "JobBatchName = Pt-${ptj}To${ptjmax}_\$(Cluster)" \
     -append "queue $queue"
 done
