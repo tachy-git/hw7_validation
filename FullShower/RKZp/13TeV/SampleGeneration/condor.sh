c765278ca4e89a3d69646069a6a9c21374c2d508
Taehee Kim <eikz.il2ljlpl@gmail.com>
2025-01-03 17:22:32 +0900

    change the settings for kisti server

--- FullShower/RKZp/13TeV/SampleGeneration/condor.sh
+++ FullShower/RKZp/13TeV/SampleGeneration/condor.sh
@@ -4,51 +4,103 @@ process=${1}
 sample=${2}
 campaign=${3}
 zpmass=${4}
+coupling=${5}
 
 if [ "$campaign" == "RunIISummer20UL16" ]; then
-    CMSSW=("CMSSW_10_6_19_patch3" "CMSSW_10_6_17_patch1" "CMSSW_10_6_17_patch1" "CMSSW_8_0_36_UL_patch1" "CMSSW_10_6_17_patch1" "CMSSW_10_6_25")
+	CMSSW=("CMSSW_10_6_19_patch3" "CMSSW_10_6_17_patch1" "CMSSW_10_6_17_patch1" "CMSSW_8_0_36_UL_patch1" "CMSSW_10_6_17_patch1" "CMSSW_10_6_25")
 elif [ "$campaign" == "RunIISummer20UL16APV" ]; then
-    CMSSW=("CMSSW_10_6_19_patch3" "CMSSW_10_6_17_patch1" "CMSSW_10_6_17_patch1" "CMSSW_8_0_36_UL_patch1" "CMSSW_10_6_17_patch1" "CMSSW_10_6_25")
+	CMSSW=("CMSSW_10_6_19_patch3" "CMSSW_10_6_17_patch1" "CMSSW_10_6_17_patch1" "CMSSW_8_0_36_UL_patch1" "CMSSW_10_6_17_patch1" "CMSSW_10_6_25")
 elif [ "$campaign" == "RunIISummer20UL17" ]; then
-    CMSSW=("CMSSW_10_6_19_patch3" "CMSSW_10_6_17_patch1" "CMSSW_10_6_17_patch1" "CMSSW_9_4_14_UL_patch1" "CMSSW_10_6_17_patch1" "CMSSW_10_6_20")
+	CMSSW=("CMSSW_10_6_19_patch3" "CMSSW_10_6_17_patch1" "CMSSW_10_6_17_patch1" "CMSSW_9_4_14_UL_patch1" "CMSSW_10_6_17_patch1" "CMSSW_10_6_20")
 elif [ "$campaign" == "RunIISummer20UL18" ]; then
-    CMSSW=("CMSSW_10_6_19_patch3" "CMSSW_10_6_17_patch1" "CMSSW_10_6_17_patch1" "CMSSW_10_2_16_UL" "CMSSW_10_6_17_patch1" "CMSSW_10_6_20")
+	CMSSW=("CMSSW_10_6_19_patch3" "CMSSW_10_6_17_patch1" "CMSSW_10_6_17_patch1" "CMSSW_10_2_16_UL" "CMSSW_10_6_17_patch1" "CMSSW_10_6_20")
 else
-    echo "Wrong Campaign: $campaign"
-    echo "You should select among RunIISummer20UL16 RunIISummer20UL16APV RunIISummer20UL17 RunIISummer20UL18"
-    exit 1
+	echo "Wrong Campaign: $campaign"
+	echo "You should select among RunIISummer20UL16 RunIISummer20UL16APV RunIISummer20UL17 RunIISummer20UL18"
+	exit 1
 fi
 RUNS=("GEN" "SIM" "DIGIPremix" "HLT" "RECO" "MiniAODv2")
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 export SCRAM_ARCH=slc7_amd64_gcc700
 
-WD="/data6/Users/taehee/HerwigWD/hw7_validation/FullShower/HAHM/13TeV/SampleGeneration"
-outputdir="\/gv0\/Users\/taehee\/HerwigSample"
-mkdir -p /gv0/Users/taehee/HerwigSample/samples/${campaign}/MZp-${zpmass}/${sample}
-mkdir -p tmp/${campaign}/MZp-${zpmass}/${sample}
+WD="/cms/ldap_home/taehee/HerwigWD/hw7_validation/FullShower/RKZp/13TeV/SampleGeneration"
+basedir_="\/cms_scratch\/taehee\/HerwigSample\/samples_nEvt-100000"
+basedir="/cms_scratch/taehee/HerwigSample/samples_nEvt-100000"
+outputdir_="${campaign}\/MZp-${zpmass}\/gbb-${coupling}\/${sample}"
+outputdir="${campaign}/MZp-${zpmass}/gbb-${coupling}/${sample}"
+mkdir -p ${basedir}/${outputdir}
+mkdir -p ${WD}/tmp/${outputdir}
+mkdir -p /cms_scratch/taehee/HerwigSample/samples_nEvt-100000/${outputdir}
 
+runStep=-1
 for ((i = 0; i < ${#RUNS[@]}; i++)); do
-    output=${RUNS[$i]}
-    if [ -s "/gv0/Users/taehee/HerwigSample/samples/${campaign}/MZp-${zpmass}/${sample}/${output}_${process}.root" ];then
-        echo "/gv0/Users/taehee/HerwigSample/samples/${campaign}/MZp-${zpmass}/${sample}/${output}_${process}.root: File exists... pass"
-        continue
-    fi
+	output=${RUNS[$i]}
+	outputFile="${basedir}/${outputdir}/${output}_${process}.root"
+	if [[ $output != "GEN" ]]; then
+		input=${RUNS[$((i-1))]}
+		inputFile="${basedir}/${outputdir}/${input}_${process}.root"
+	fi
 
+	if [[ -f "${outputFile}" ]]; then
+		logFile="${WD}/tmp/${outputdir}/${output}_${process}.log"
+		if grep -q "MessageLogger Summary" "${logFile}" && ! grep -q "Begin Fatal Exception" "${logFile}"; then
+			echo "${outputFile}: Exists... pass the process"
+			runStep=$i
+			if [[ $output != "GEN" ]]; then
+				rm -f ${inputFile}
+			fi
+		else
+			rm -f "${outputFile}"
+			echo "Remove ${outputFile} since the error has detected."
+		fi
+	fi
+
+done
+
+for ((i = runStep + 1; i < ${#RUNS[@]}; i++)); do
+    output=${RUNS[$i]}
+	outputFile="${basedir}/${outputdir}/${output}_${process}.root"
+    echo "Running ${outputdir}/${output}_${process}..."
     if [[ $output != "GEN" ]]; then
         input=${RUNS[$((i-1))]}
+		inputFile="${basedir}/${outputdir}/${input}_${process}.root"
     fi
-    cd /data9/Users/taehee/${CMSSW[$i]}/src
+
+	cd ${WD}
+    sed -e "s/__OUTPUT__/${basedir_}\/${outputdir_}\/${output}_${process}/g" "files_cfg/${campaign}${output}_cfg.py" > "tmp/${outputdir}/${output}_${process}.py"
+    if [[ $output == "GEN" ]]; then
+        if [ ! -f "/cms_scratch/taehee/HerwigSample/hw_nEvt-100000/MZp-${zpmass}/gbb-${coupling}/${sample}/${process}/LHC.hepmc" ];then
+            echo "${outputdir}/${output}_${process}: LHC.hepmc does not exist... exit"
+            exit 1
+        fi
+        sed -i "s/__INPUT__/\/cms_scratch\/taehee\/HerwigSample\/hw_nEvt-100000\/MZp-${zpmass}\/gbb-${coupling}\/${sample}\/${process}\/LHC/g" "tmp/${outputdir}/${output}_${process}.py"
+        sed -i "s/__RANDOM__/${process}/g" "tmp/${outputdir}/${output}_${process}.py"
+    else
+        if [ ! -f "${inputFile}" ]; then
+            echo "${inputFile}: no input files... exit"
+            exit
+        fi
+        sed -i "s/__INPUT__/${basedir_}\/${outputdir_}\/${input}_${process}/g" "tmp/${outputdir}/${output}_${process}.py"
+    fi
+
+    cd ${WD}/CMSSW/${CMSSW[$i]}/src
     eval `scram runtime -sh`
     scram b
-    cd $WD
-    sed -e "s/__OUTPUT__/${outputdir}\/samples\/${campaign}\/MZp-${zpmass}\/${sample}\/${output}_${process}/g" "files_cfg/${campaign}${output}_cfg.py" > "tmp/${campaign}/MZp-${zpmass}/${sample}/${output}_${process}.py"
-    if [[ $output == "GEN" ]]; then
-        sed -i "s/__INPUT__/${outputdir}\/hw\/MZp-${zpmass}\/${sample}\/$process\/LHC/g" "tmp/${campaign}/MZp-${zpmass}/${sample}/${output}_${process}.py"
-        sed -i "s/__RANDOM__/${process}/g" "tmp/${campaign}/MZp-${zpmass}/${sample}/${output}_${process}.py"
-    else
-        sed -i "s/__INPUT__/${outputdir}\/samples\/${campaign}\/MZp-${zpmass}\/${sample}\/${input}_${process}/g" "tmp/${campaign}/MZp-${zpmass}/${sample}/${output}_${process}.py"
-    fi
-    cmsRun tmp/${campaign}/MZp-${zpmass}/${sample}/${output}_${process}.py &> tmp/${campaign}/MZp-${zpmass}/${sample}/${output}_${process}.log
+
+	cd ${WD}
+	logFile="${WD}/tmp/${outputdir}/${output}_${process}.log"
+    cmsRun ${WD}/tmp/${outputdir}/${output}_${process}.py &> ${logFile}
+	echo "Finished running ${outputdir}/${output}_${process}"
+
+	if grep -q "MessageLogger Summary" "${logFile}" && ! grep -q "Begin Fatal Exception" "${logFile}"; then
+		echo "${outputFile}: Exists... pass the process"
+		if [[ $output != "GEN" ]]; then
+			rm -f ${inputFile}
+		fi
+	else
+		rm -f "${outputFile}"
+		echo "Remove ${outputFile} since the error has detected."
+	fi
 
 done
--- FullShower/RKZp/13TeV/SampleGeneration/submit_condor.sh
+++ FullShower/RKZp/13TeV/SampleGeneration/submit_condor.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-SAMPLES=("M-50_Pt-300To320_1317762" "M-50_Pt-320To340_1317763")
+SAMPLES=("Pt-200To210_4767501" "Pt-210To220_4767502" "Pt-220To230_4767503" "Pt-230To250_4767504" "Pt-250To270_4767505" "Pt-270To300_4767506" "Pt-300To400_4767507" "Pt-400To9999_4767508" "Pt-200To210_4767608" "Pt-210To220_4767609" "Pt-220To230_4767610" "Pt-230To250_4767611" "Pt-250To270_4767612" "Pt-270To300_4767613" "Pt-300To400_4767614" "Pt-400To9999_4767615")
 
+SAMPLES=("Pt-140To145_4767530" "Pt-145To150_4767531" "Pt-150To160_4767532" "Pt-160To170_4767533" "Pt-170To180_4767534" "Pt-180To200_4767535" "Pt-200To240_4767536" "Pt-240To300_4767537" "Pt-300To9999_4767538" "Pt-140To145_4767590" "Pt-145To150_4767591" "Pt-150To160_4767592" "Pt-160To170_4767593" "Pt-170To180_4767594" "Pt-180To200_4767595" "Pt-200To240_4767596" "Pt-240To300_4767597" "Pt-300To9999_4767598")
+
+zpmass=50
+coupling="0p1"
 campaign="RunIISummer20UL16"
 # "RunIISummer20UL16" "RunIISummer20UL16APV" "RunIISummer20UL17" "RunIISummer20UL18"
 
@@ -9,18 +13,25 @@ cat <<EOT > submit_condor.jds
 universe        = vanilla
 executable      = condor.sh
 arguments       = \$(Process) \$(sample) \$(campaign)
-output          = job.\$(Cluster).\$(Process).out
-error           = job.\$(Cluster).\$(Process).err
-log             = job.\$(Cluster).\$(Process).log
-x509userproxy   = /tmp/x509up_u5592
+output          = joblog/job.\$(Cluster).\$(Process).out
+error           = joblog/job.\$(Cluster).\$(Process).err
+log             = joblog/job.\$(Cluster).\$(Process).log
+accounting_group = group_alice
++SingularityImage = "/cvmfs/singularity.opensciencegrid.org/opensciencegrid/osgvo-el7:latest"
++SingularityBindCVMFS = True
++SingularityBind  = "/cvmfs,/cms,/share,/cms_scratch"
+x509userproxy = /tmp/x509up_u556951238
+stream_output   = True
+stream_error    = True
+request_memory = 4GB
 EOT
 
 for ((i=0; i<${#SAMPLES[@]}; i++)); do
     sample=${SAMPLES[i]}
-    queue=$(ls /gv0/Users/taehee/HerwigSample/hw/$sample | wc -l)
-    echo "Submitting job for sample production: $sample"
+    queue=$(ls /cms_scratch/taehee/HerwigSample/hw_nEvt-100000/MZp-$zpmass/gbb-$coupling/$sample | wc -l)
+    echo "Submitting jobs for sample production: $campaign"_"MZp-$zpmass"_"$sample"
     condor_submit submit_condor.jds \
-        -append "arguments = \$(Process) $sample $campaign" \
-        -append "JobBatchName = SAMPLE_$sample" \
+        -append "arguments = \$(Process) $sample $campaign $zpmass $coupling" \
+        -append "JobBatchName = "$campaign"_MZp-"$zpmass"_"$sample \
         -append "queue $queue"
 done
